f6d038eb8562c850a9706354e55cb5d12e6cf820
sq <wweerrbb@gmail.com>
2023-08-23 16:27:11 -0400

    initial commit

---
+++ ot.py
@@ -0,0 +1,24 @@
+def main():
+    print("OT!")
+    print(delete("abcdefg", 0, 4))
+# def test():
+#     return "sf"+"fsdf"
+def OT(stale, pos, operations):
+    for op in operations:
+
+        return ""
+
+def insert(stale, newString):
+    return stale+newString
+
+def delete(stale, cursor, count):
+    return stale.replace(stale[cursor:count], '')
+
+def skip():
+    return
+
+def isValid(stale, latest, otjson):
+    return latest == OT(stale, otjson)
+
+if __name__ == "__main__":
+    main()
