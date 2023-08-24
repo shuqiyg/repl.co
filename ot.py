4e6999252da3ba96b73fa234bff05f0f43a6074d
sq <wweerrbb@gmail.com>
2023-08-24 16:11:28 -0400

    isValid()

--- ot.py
+++ ot.py
@@ -1,24 +1,57 @@
 def main():
+    stale = "I love you ok"
     print("OT!")
-    print(delete("abcdefg", 0, 4))
+    stale = stale[:0] + "weird insertion" + stale[0:]
+    print(stale)
+    valid = isValid(
+        'Repl.it uses operational transformations to keep everyone in a multiplayer repl in sync.',
+        'Repl.it uses operational transformations.',
+        [{"op": "skip", "count": 40}, {"op": "delete", "count": 47}]
+    )
+    print(valid)
 # def test():
 #     return "sf"+"fsdf"
-def OT(stale, pos, operations):
-    for op in operations:
+# def OT(stale, pos, operations):
+#     if operations:      
+#         for op in operations:
+#             if(op == 'insert'):
+#                 insert(stale, operations["chars"])
+#             elif(op == 'delete'):
+#                 delete(stale, pos, operations["count"])
+#             else:
+#                 skip(pos, operations["count"])
+#     return stale
 
-        return ""
+# def insert(stale, newString):
+#     return stale+newString
 
-def insert(stale, newString):
-    return stale+newString
+# def delete(stale, cursor, count):
+#     return stale.replace(stale[cursor:count], '')
 
-def delete(stale, cursor, count):
-    return stale.replace(stale[cursor:count], '')
+# def skip(cursor, count):
+#     return cursor + count - 1
 
-def skip():
-    return
-
-def isValid(stale, latest, otjson):
-    return latest == OT(stale, otjson)
+def isValid(stale, latest, otjson): 
+    if not otjson:
+        return True
+    cursor = 0
+    for ot in otjson:
+            if(ot['op'] == 'insert'):
+                # insert(stale, operations["chars"])
+                stale = stale[:cursor] + ot['chars'] + stale[cursor:]
+            elif(ot['op']== 'delete'):
+                # delete(stale, pos, operations["count"])
+                if(len(stale) - cursor < ot['count']):
+                    return False
+                stale = stale.replace(stale[cursor:ot['count']], '')
+            else:
+                # skip(pos, operations["count"])
+                if cursor + ot['count'] > len(stale):
+                    return False
+                else:
+                    cursor += ot['count']
+    
+    return latest == stale
 
 if __name__ == "__main__":
     main()
